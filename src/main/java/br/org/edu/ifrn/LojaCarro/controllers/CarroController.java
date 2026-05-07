1de16265bf06ce940f953a38859d305ad6cfa5e7
diego oliveira <diego.oliveira@escolar.ifrn.edu.br>
2026-05-07 10:15:35 -0300

    feature deletar, atualizar e pesquisar carro adicionadas

--- src/main/java/br/org/edu/ifrn/LojaCarro/controllers/CarroController.java
+++ src/main/java/br/org/edu/ifrn/LojaCarro/controllers/CarroController.java
@@ -1,12 +1,14 @@
+
 package br.org.edu.ifrn.LojaCarro.controllers;
 
 import br.org.edu.ifrn.LojaCarro.model.Carro;
 import br.org.edu.ifrn.LojaCarro.services.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/carro")
@@ -15,9 +17,39 @@ public class CarroController {
     @Autowired
     private CarroService carroService;
 
-    @GetMapping
-    public void salvarCarro(@RequestBody Carro c){
-        carroService.save(c);
+    // Salvar carro (corrigido para POST)
+    @PostMapping
+    public ResponseEntity<Carro> salvarCarro(@RequestBody Carro c) {
+        Carro savedCarro = carroService.save(c);
+        return ResponseEntity.ok(savedCarro);
     }
 
+    // Atualizar carro (por ID)
+    @PutMapping("/{id}")
+    public ResponseEntity<Carro> atualizarCarro(@PathVariable Long id, @RequestBody Carro c) {
+        c.setId(id);  // Define o ID no objeto
+        Carro updatedCarro = carroService.update(c);
+        return ResponseEntity.ok(updatedCarro);
+    }
+
+    // Deletar carro (por ID)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCarro(@PathVariable Long id) {
+        carroService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Pesquisar carro por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Carro> pesquisarCarroPorId(@PathVariable Long id) {
+        Optional<Carro> carro = carroService.findById(id);
+        return carro.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    // Pesquisar todos os carros
+    @GetMapping
+    public ResponseEntity<List<Carro>> pesquisarTodosCarros() {
+        List<Carro> carros = carroService.findAll();
+        return ResponseEntity.ok(carros);
+    }
 }
