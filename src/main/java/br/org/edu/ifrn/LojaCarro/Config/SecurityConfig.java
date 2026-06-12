26028b8efea04fe2b53ad7397f7fa76a8d7e027e
JG1511 <joaotsi2024@gmail.com>
2026-06-11 22:34:40 -0300

    adicionando o pit

---
+++ src/main/java/br/org/edu/ifrn/LojaCarro/Config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package br.org.edu.ifrn.LojaCarro.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()   // rotas públicas
+                        .requestMatchers("/carro/salvar").hasRole("ADMIN")  // só admin pode salvar
+                        .requestMatchers("/carro/{id}").hasRole("ADMIN")    // atualizar e deletar só admin
+                        .requestMatchers("/carro").hasAnyRole("USER","ADMIN") // GET /carro todos podem ver
+                        .anyRequest().authenticated()
+                )
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
